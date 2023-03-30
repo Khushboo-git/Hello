464f7cd5b37813b2a33b025f658cfe6789179a80
Dummy <moorkhilifeinamerica@gmail.com>
2023-03-30 10:03:26 -0700

    First commit

---
+++ src/Module1/world.java
@@ -0,0 +1,11 @@
+package Module1;
+
+public class world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Khushboo
+ *
+ */
+module Hello {
+}
